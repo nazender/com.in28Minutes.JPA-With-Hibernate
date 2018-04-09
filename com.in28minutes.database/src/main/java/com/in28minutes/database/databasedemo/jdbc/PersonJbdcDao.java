fc18aad690b668f609f8dc836f9276827575c8d1
Anton Petrov <petrov_a@aptekaural.local>
2018-04-09 14:48:37 +0500

    step 3-29

--- com.in28minutes.database/src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJbdcDao.java
+++ com.in28minutes.database/src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJbdcDao.java
@@ -15,6 +15,19 @@ public class PersonJbdcDao {
     JdbcTemplate jdbcTemplate;
 
     public List<Person> findAll() {
-        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+        return jdbcTemplate.query(
+                "select * from person", new BeanPropertyRowMapper(Person.class)
+        );
+    }
+
+    public Person findById(int id) {
+        return jdbcTemplate.queryForObject(
+                "select * from person where id=?", new Object[] { id }, new BeanPropertyRowMapper<Person>(Person.class)
+        );
+    }
+    public int deleteById(int id) {
+        return jdbcTemplate.update(
+                "delete from person where id=?", new Object[] { id }
+        );
     }
 }
