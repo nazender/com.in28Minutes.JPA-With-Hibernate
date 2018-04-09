4cf47e1f8b73fcc186623989ae990267eb712f43
Anton Petrov <petrov_a@aptekaural.local>
2018-04-09 14:13:04 +0500

    step 3-26

---
+++ com.in28minutes.database/src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJbdcDao.java
@@ -0,0 +1,20 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import java.util.List;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonJbdcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+    }
+}
