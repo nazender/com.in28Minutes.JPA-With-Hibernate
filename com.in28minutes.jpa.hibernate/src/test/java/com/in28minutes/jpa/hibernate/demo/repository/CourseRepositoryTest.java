e5d745b978691107fc4c5806f06970c6cd35f347
Anton Petrov <petrov_a@aptekaural.local>
2018-04-11 13:18:24 +0500

    step 5-54

--- com.in28minutes.jpa.hibernate/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
+++ com.in28minutes.jpa.hibernate/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -36,4 +36,22 @@ public class CourseRepositoryTest {
         assertNull(repository.findById(10002L));
     }
 
+    @Test
+    @DirtiesContext
+    public void save_basic() {
+
+        // get a course
+        Course course = repository.findById(10001L);
+        //assertEquals("JPA in 50 Steps", course.getName());
+
+        // update details
+        course.setName("JPA in 50 Steps - Updated");
+
+        repository.save(course);
+
+        // check the value
+        Course course1 = repository.findById(10001L);
+        assertEquals("JPA in 50 Steps - Updated", course1.getName());
+    }
+
 }
