781bd4d4a37019aed9a662ab29583b88048d5a91
Anton Petrov <petrov_a@aptekaural.local>
2018-04-11 12:48:31 +0500

    step 5-52

--- com.in28minutes.jpa.hibernate/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
+++ com.in28minutes.jpa.hibernate/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -2,10 +2,13 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,4 +28,12 @@ public class CourseRepositoryTest {
         Course course = repository.findById(10001L);
         assertEquals("JPA in 50 Steps", course.getName());
     }
+
+    @Test
+    @DirtiesContext
+    public void deleteById_basic() {
+        repository.deleteById(10002L);
+        assertNull(repository.findById(10002L));
+    }
+
 }
