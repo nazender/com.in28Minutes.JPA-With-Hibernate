3a8a69fcf001a25d11f58df1d6d4708c0738952d
Anton Petrov <petrov_a@aptekaural.local>
2018-04-20 09:29:58 +0500

    step 5-69

--- com.in28minutes.jpa.hibernate/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoJpaApplication.java
+++ com.in28minutes.jpa.hibernate/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoJpaApplication.java
@@ -2,6 +2,7 @@ package com.in28minutes.jpa.hibernate.demo;
 
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
 import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
+import com.in28minutes.jpa.hibernate.demo.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +17,26 @@ public class DemoJpaApplication implements CommandLineRunner {
 	@Autowired
 	private CourseRepository repository;
 
+	@Autowired
+	private StudentRepository studentRepository;
+
 	@Override
 	public void run(String... arg0) throws Exception {
 //		repository.playWithEntityManager();
-		logger.info("Find all -> {}", repository.findAll());
-		logger.info("Find by name like Spring -> {}", repository.getByNameLike("%Spring%"));
-		logger.info("Find by name like Spring -> {}", repository.getByNameLikeNative("%Spring%"));
+//		logger.info("Find all -> {}", repository.findAll());
+//		logger.info("Find by name like Spring -> {}", repository.getByNameLike("%Spring%"));
+//		logger.info("Find by name like Spring -> {}", repository.getByNameLikeNative("%Spring%"));
 
-		Course course = repository.findById(10001L);
-		if (course != null) {
-			course.setName("Hello");
-			repository.save(course);
-		}
+//		Course course = repository.findById(10001L);
+//		if (course != null) {
+//			course.setName("Hello");
+//			repository.save(course);
+//		}
 
-		Course course1 = new Course("Heelo1");
-		repository.save(course1);
+//		Course course1 = new Course("Heelo1");
+//		repository.save(course1);
+
+		studentRepository.saveStudentWithPassport();
 
 	}
 
