906879f0137d62fa8b55e5d15bbe26b22a58b0fa
Anton Petrov <petrov_a@aptekaural.local>
2018-05-10 08:52:46 +0500

    step 8-89

--- com.in28minutes.jpa.hibernate/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
+++ com.in28minutes.jpa.hibernate/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
@@ -18,6 +18,7 @@ public class Student {
     private Passport passport;
 
     @ManyToMany
+    @JoinTable(name = "STUDENT_COURSE", joinColumns = @JoinColumn(name = "STUDENT_ID"), inverseJoinColumns = @JoinColumn(name = "COURSE_ID"))
     private List<Course> courses = new ArrayList<>();
 
     protected Student() {
